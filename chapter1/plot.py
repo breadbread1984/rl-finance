4616805dbcc6ae77111f41087699a384b6d6b20a
yi.xie <yi.xie@enflame-tech.com>
2021-08-22 10:55:02 +0800

    update

--- chapter1/plot.py
+++ chapter1/plot.py
@@ -7,9 +7,9 @@ from process3 import main as proc3;
 
 def main():
   time = [t for t in range(101)];
-  results1 = proc1(step_time = 100);
-  results2 = proc2(step_time = 100);
-  results3 = proc3(step_time = 100);
+  results1 = proc1(step_time = 100, x0 = 100., a1 = 0.25, L = 100.);
+  results2 = proc2(step_time = 100, x0 = 100., a2 = 0.75);
+  results3 = proc3(step_time = 100, x0 = 100., a3 = 1.);
   plt.plot(time, results1, label = 'process1', color = 'red');
   plt.plot(time, results2, label = 'process2', color = 'green');
   plt.plot(time, results3, label = 'process3', color = 'blue');
--- chapter1/process1.py
+++ chapter1/process1.py
@@ -13,11 +13,11 @@ def main(x0 = 1., step_time = 20, a1 = 0.25, L = 0):
     results.append(x);
   return results;
 
-def step(x0, a1, L):
+def step(x_t, a1, L):
 
-  up = tfp.distributions.Bernoulli(probs = tf.math.sigmoid(-a1 * (x0-L))).sample();
-  x1 = tf.where(tf.math.equal(up, 1), x0 + 1, x0 - 1);
-  return x1;
+  up = tfp.distributions.Bernoulli(probs = tf.math.sigmoid(a1 * (L-x_t))).sample();
+  x_tp1 = tf.where(tf.math.equal(up, 1), x_t + 1, x_t - 1);
+  return x_tp1;
 
 if __name__ == "__main__":
   print(main());
